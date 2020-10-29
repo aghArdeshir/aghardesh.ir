227aa4774a9f51db0e727c4a28b4f4a7bc4258ba
aghArdeshir <ardeshireo@gmail.com>
2020-10-29 11:53:21 +0330

    add a new song

--- songs/songs.mjs
+++ songs/songs.mjs
@@ -1,4 +1,5 @@
 import aghArdeshirBadAzBadLyrics from "./aghArdeshir - Ba'd Az Bad/ba'd-az-bad.lyrics.mjs";
+import nishkhandSarbasteyeYekSarbazLyrics from "./Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.lyrics.mjs";
 
 export const songs = {
   aghArdeshir: {
@@ -7,6 +8,15 @@ export const songs = {
         "songs/aghArdeshir - Ba'd Az Bad/ba'd-az-bad.original.1800x1800.png",
       songUrl: "songs/aghArdeshir - Ba'd Az Bad/aghArdeshir - Ba'd Az Bad.mp3",
       lyrics: aghArdeshirBadAzBadLyrics,
+    },
+  },
+  nishkhand: {
+    sarbasteyeYekSarbaz: {
+      coverUrl:
+        "songs/Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.cover.1200x1200.png",
+      songUrl:
+        "songs/Nishkhand - Sarbasteye Yek Sarbaz/Nishkhand - Sarbasteye Yek Sarbaz 128.mp3",
+      lyrics: nishkhandSarbasteyeYekSarbazLyrics,
     },
   },
 };
