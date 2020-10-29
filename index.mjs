0fdc38911a7adab3da315e4f1da85e3efcd140be
aghArdeshir <ardeshireo@gmail.com>
2020-10-29 13:55:57 +0330

    use json as lyrics

--- index.mjs
+++ index.mjs
@@ -1,88 +1,93 @@
 import { songs } from "./songs/songs.mjs";
+
 const song = songs.nishkhand.sarbasteyeYekSarbaz;
 
-document.getElementsByClassName("cover-art")[0].src = song.coverUrl;
-document.body.style.backgroundColor = song.bgColor;
-document.body.style.color = song.textColor;
-document.querySelector(".title-persian").textContent = song.title;
-document.querySelector(".artist-persian").textContent = song.artist;
+fetch(song.lyricsUrl)
+  .then((res) => res.json())
+  .then((lyrics) => {
+    document.getElementsByClassName("cover-art")[0].src = song.coverUrl;
+    document.body.style.backgroundColor = song.bgColor;
+    document.body.style.color = song.textColor;
+    document.querySelector(".title-persian").textContent = song.title;
+    document.querySelector(".artist-persian").textContent = song.artist;
 
-const cssTransition = "all 0.4s ease-out";
-const cssTop = "0px";
-const cssFontSize = "18px";
-const cssSmallFontSize = "0px";
+    const cssTransition = "all 0.4s ease-out";
+    const cssTop = "0px";
+    const cssFontSize = "18px";
+    const cssSmallFontSize = "0px";
 
-const lyricsDom = document.getElementById("lyrics");
-const lyricsBackupDom = document.getElementById("lyrics-backup");
+    const lyricsDom = document.getElementById("lyrics");
+    const lyricsBackupDom = document.getElementById("lyrics-backup");
 
-var wavesurfer = WaveSurfer.create({
-  container: "#waveform",
-});
+    var wavesurfer = WaveSurfer.create({
+      container: "#waveform",
+    });
 
-let lyricsText = "";
-let lyricsBackupText = "";
+    let lyricsText = "";
+    let lyricsBackupText = "";
 
-function setLyricsText(text = "") {
-  if (lyricsText !== text) {
-    lyricsText = text || "";
+    function setLyricsText(text = "") {
+      if (lyricsText !== text) {
+        lyricsText = text || "";
 
-    lyricsDom.style.transition = "";
-    lyricsDom.style.top = "90px";
-    lyricsDom.style.fontSize = cssSmallFontSize;
-    lyricsDom.style.opacity = "0.5";
+        lyricsDom.style.transition = "";
+        lyricsDom.style.top = "90px";
+        lyricsDom.style.fontSize = cssSmallFontSize;
+        lyricsDom.style.opacity = "0.5";
 
-    lyricsDom.innerHTML = lyricsText;
+        lyricsDom.innerHTML = lyricsText;
+
+        setTimeout(() => {
+          lyricsDom.style.transition = cssTransition;
+          lyricsDom.style.top = cssTop;
+          lyricsDom.style.fontSize = cssFontSize;
+          lyricsDom.style.opacity = "1";
+        }, 400);
+      }
+    }
+
+    function setLyricsBackupText(text = "") {
+      if (lyricsBackupText !== text) {
+        lyricsBackupText = text || "";
+
+        lyricsBackupDom.style.transition = "";
+        lyricsBackupDom.style.top = "40px";
+        lyricsBackupDom.style.fontSize = cssFontSize;
+        lyricsBackupDom.style.opacity = "1";
+
+        lyricsBackupDom.innerHTML = lyricsBackupText;
+
+        setTimeout(() => {
+          lyricsBackupDom.style.transition = cssTransition;
+          lyricsBackupDom.style.top = cssTop;
+          lyricsBackupDom.style.fontSize = cssSmallFontSize;
+          lyricsBackupDom.style.opacity = "0.5";
+        }, 400);
+      }
+    }
+
+    wavesurfer.load(song.songUrl);
+    wavesurfer.on("audioprocess", function (currentTime) {
+      const index =
+        lyrics.findIndex((member) => member.time - 0.4 >= currentTime) - 1;
+
+      if (index < 0) {
+        lyricsDom.innerHTML = "";
+        return;
+      }
+
+      if (index === 0 && lyrics[index].time - 0.4 > currentTime) {
+        lyricsDom.innerHTML = "";
+        return;
+      }
+
+      setLyricsText(lyrics[index].text);
+      if (lyrics[index - 1]) {
+        setLyricsBackupText(lyrics[index - 1].text);
+      }
+    });
 
     setTimeout(() => {
-      lyricsDom.style.transition = cssTransition;
-      lyricsDom.style.top = cssTop;
-      lyricsDom.style.fontSize = cssFontSize;
-      lyricsDom.style.opacity = "1";
-    }, 400);
-  }
-}
-
-function setLyricsBackupText(text = "") {
-  if (lyricsBackupText !== text) {
-    lyricsBackupText = text || "";
-
-    lyricsBackupDom.style.transition = "";
-    lyricsBackupDom.style.top = "40px";
-    lyricsBackupDom.style.fontSize = cssFontSize;
-    lyricsBackupDom.style.opacity = "1";
-
-    lyricsBackupDom.innerHTML = lyricsBackupText;
-
-    setTimeout(() => {
-      lyricsBackupDom.style.transition = cssTransition;
-      lyricsBackupDom.style.top = cssTop;
-      lyricsBackupDom.style.fontSize = cssSmallFontSize;
-      lyricsBackupDom.style.opacity = "0.5";
-    }, 400);
-  }
-}
-
-wavesurfer.load(song.songUrl);
-wavesurfer.on("audioprocess", function (currentTime) {
-  const index =
-    song.lyrics.findIndex((member) => member.time - 0.4 >= currentTime) - 1;
-
-  if (index < 0) {
-    lyricsDom.innerHTML = "";
-    return;
-  }
-
-  if (index === 0 && song.lyrics[index].time - 0.4 > currentTime) {
-    lyricsDom.innerHTML = "";
-    return;
-  }
-
-  setLyricsText(song.lyrics[index].text);
-  if (song.lyrics[index - 1]) {
-    setLyricsBackupText(song.lyrics[index - 1].text);
-  }
-});
-
-setTimeout(() => {
-  wavesurfer.play();
-}, 5000);
+      wavesurfer.play();
+    }, 5000);
+  });
--- songs/songs.mjs
+++ songs/songs.mjs
@@ -1,13 +1,10 @@
-import aghArdeshirBadAzBadLyrics from "./aghArdeshir - Ba'd Az Bad/ba'd-az-bad.lyrics.mjs";
-import nishkhandSarbasteyeYekSarbazLyrics from "./Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.lyrics.mjs";
-
 export const songs = {
   aghArdeshir: {
     badAzBad: {
       coverUrl:
         "songs/aghArdeshir - Ba'd Az Bad/ba'd-az-bad.original.1800x1800.png",
       songUrl: "songs/aghArdeshir - Ba'd Az Bad/aghArdeshir - Ba'd Az Bad.mp3",
-      lyrics: aghArdeshirBadAzBadLyrics,
+      lyricsUrl: "songs/aghArdeshir - Ba'd Az Bad/ba'd-az-bad.lyrics.json",
       bgColor: "#252527",
       textColor: "white",
       title: "بعد از بد",
@@ -20,7 +17,8 @@ export const songs = {
         "songs/Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.cover.1200x1200.png",
       songUrl:
         "songs/Nishkhand - Sarbasteye Yek Sarbaz/Nishkhand - Sarbasteye Yek Sarbaz 128.mp3",
-      lyrics: nishkhandSarbasteyeYekSarbazLyrics,
+      lyricsUrl:
+        "songs/Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.lyrics.json",
       bgColor: "white",
       textColor: "black",
       title: "سربسته‌ی یک سرباز",
