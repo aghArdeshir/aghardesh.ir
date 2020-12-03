60371cc71b3675a87c352a1fdbcea1f64872fef2
aghArdeshir <ardeshireo@gmail.com>
2020-12-03 15:48:00 +0330

    minor improvement on job-record and code formattin g

--- job-resume/job-record.mjs
+++ job-resume/job-record.mjs
@@ -1,4 +1,4 @@
-import { colors } from './colors.mjs';
+import { colors } from "./colors.mjs";
 
 let counter = 0;
 
@@ -6,17 +6,17 @@ class JobRecordCustomElement extends HTMLDivElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: "open" });
 
-    this.wrapper = document.createElement('div');
-    this.wrapper.setAttribute('class', 'job-record');
+    this.wrapper = document.createElement("div");
+    this.wrapper.setAttribute("class", "job-record");
 
     this.attachJobStartDate();
     this.attachTimeline();
     this.attachJobEndDate();
     this.attachJobDetalis();
 
-    const style = document.createElement('style');
+    const style = document.createElement("style");
     style.textContent = `
     .job-record {
       border-top: 1px solid;
@@ -74,54 +74,56 @@ class JobRecordCustomElement extends HTMLDivElement {
   }
 
   attachJobStartDate() {
-    const jobStartDate = document.createElement('span');
-    jobStartDate.setAttribute('class', 'job-start-date');
-    jobStartDate.textContent = this.getAttribute('start-date');
+    const jobStartDate = document.createElement("span");
+    jobStartDate.setAttribute("class", "job-start-date");
+    jobStartDate.textContent = this.getAttribute("start-date");
     this.wrapper.appendChild(jobStartDate);
   }
 
   attachTimeline() {
-    const timeline = document.createElement('span');
-    timeline.setAttribute('class', 'timeline');
+    const timeline = document.createElement("span");
+    timeline.setAttribute("class", "timeline");
     this.wrapper.appendChild(timeline);
   }
 
   attachJobEndDate() {
-    const jobEndDate = document.createElement('span');
-    jobEndDate.setAttribute('class', 'job-end-date');
-    jobEndDate.textContent = this.getAttribute('end-date');
+    const jobEndDate = document.createElement("span");
+    jobEndDate.setAttribute("class", "job-end-date");
+    jobEndDate.textContent = this.getAttribute("end-date");
     this.wrapper.appendChild(jobEndDate);
   }
 
   attachJobDetalis() {
-    const jobDetailsWrapper = document.createElement('div');
-    jobDetailsWrapper.setAttribute('class', 'job-details');
+    const jobDetailsWrapper = document.createElement("div");
+    jobDetailsWrapper.setAttribute("class", "job-details");
 
-    const header = document.createElement('h5');
-    header.textContent = this.getAttribute('job-title') + ' @ ';
+    const header = document.createElement("h5");
+    header.textContent = this.getAttribute("job-title") + " @ ";
     const companyTitle = document.createElement(
-      this.getAttribute('company-link') ? 'a' : 'span'
+      this.getAttribute("company-link") ? "a" : "span"
     );
-    companyTitle.textContent = this.getAttribute('company-title');
-    if (this.getAttribute('company-link')) {
-      companyTitle.setAttribute('href', this.getAttribute('company-link'));
-      companyTitle.setAttribute('target', '_blank');
-      companyTitle.setAttribute('rel', 'noopener');
+    companyTitle.textContent = this.getAttribute("company-link")
+      ? this.getAttribute("company-title") + "🔗"
+      : this.getAttribute("company-title");
+    if (this.getAttribute("company-link")) {
+      companyTitle.setAttribute("href", this.getAttribute("company-link"));
+      companyTitle.setAttribute("target", "_blank");
+      companyTitle.setAttribute("rel", "noopener");
     }
     header.appendChild(companyTitle);
 
     jobDetailsWrapper.appendChild(header);
 
-    const location = document.createElement('span');
-    location.textContent = this.getAttribute('job-location');
+    const location = document.createElement("span");
+    location.textContent = this.getAttribute("job-location");
     jobDetailsWrapper.appendChild(location);
 
-    const jobDuration = document.createElement('div');
-    jobDuration.setAttribute('class', 'job-duration');
-    jobDuration.textContent = this.getAttribute('duration');
+    const jobDuration = document.createElement("div");
+    jobDuration.setAttribute("class", "job-duration");
+    jobDuration.textContent = this.getAttribute("duration");
     jobDetailsWrapper.appendChild(jobDuration);
 
-    const jobDetails = document.createElement('p');
+    const jobDetails = document.createElement("p");
     jobDetails.textContent = this.textContent;
     jobDetailsWrapper.appendChild(jobDetails);
 
@@ -129,6 +131,6 @@ class JobRecordCustomElement extends HTMLDivElement {
   }
 }
 
-customElements.define('job-record', JobRecordCustomElement, {
-  extends: 'div',
+customElements.define("job-record", JobRecordCustomElement, {
+  extends: "div",
 });
