b10a8958f2fd803bb05c999d5467e0abb1ac3643
aghArdeshir <ardeshireo@gmail.com>
2020-12-03 15:24:54 +0330

    add theme-cahnger buttons

--- job-resume/colors.mjs
+++ job-resume/colors.mjs
@@ -1,1 +1,1 @@
-export const colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
+export const colors = ["#264653DD", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"];
---
+++ job-resume/index.mjs
@@ -0,0 +1,47 @@
+import { colors } from "./colors.mjs";
+
+function doDefaultTheme() {
+  undoDarkTheme();
+  takeBrushFromTheChild();
+}
+
+function giveBrushToTheChild() {
+  doDefaultTheme();
+
+  document.body.classList.add("child-brush");
+
+  document.querySelector(
+    ".overview-section"
+  ).style.backgroundImage = `linear-gradient(${colors[1]}, ${colors[2]})`;
+
+  for (let i = 1; i < 5; i++) {
+    document.querySelectorAll("section")[
+      i
+    ].style.backgroundImage = `linear-gradient(to right, ${colors[i - 1]}, ${
+      colors[i]
+    })`;
+  }
+}
+
+function doDarkTheme() {
+  doDefaultTheme();
+  document.body.classList.add("dark-theme");
+}
+
+function takeBrushFromTheChild() {
+  document.body.classList.remove("child-brush");
+
+  document.querySelector(".overview-section").style.backgroundImage = `unset`;
+
+  for (let i = 1; i < 5; i++) {
+    document.querySelectorAll("section")[i].style.backgroundImage = `unset`;
+  }
+}
+
+function undoDarkTheme() {
+  document.body.classList.remove("dark-theme");
+}
+
+window.doDefaultTheme = doDefaultTheme;
+window.doDarkTheme = doDarkTheme;
+window.giveBrushToTheChild = giveBrushToTheChild;
