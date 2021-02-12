1d23196818d5e9046bd15d196b43b64f971f262c
aghArdeshir <ardeshireo@gmail.com>
2021-02-12 17:02:49 +0330

    remove unneeded code

--- job-resume/index.mjs
+++ job-resume/index.mjs
@@ -1,21 +1,10 @@
 function doDefaultTheme() {
   undoDarkTheme();
-  takeBrushFromTheChild();
 }
 
 function doDarkTheme() {
   doDefaultTheme();
   document.body.classList.add("dark-theme");
-}
-
-function takeBrushFromTheChild() {
-  document.body.classList.remove("child-brush");
-
-  document.querySelector(".overview-section").style.backgroundImage = `unset`;
-
-  for (let i = 1; i < 5; i++) {
-    document.querySelectorAll("section")[i].style.backgroundImage = `unset`;
-  }
 }
 
 function undoDarkTheme() {
